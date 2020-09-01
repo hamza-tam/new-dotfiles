2e5ca68120baaff62036d51ddd800b21dd6b4df1
Hamza Tamenaoul <hamzatamenaoul@gmail.com>
2020-09-01 19:28:51 +0200

    Added rofi menus and mapped them in i3

--- .scripts/power.sh
+++ .scripts/power.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+## Author: Hamza Tamenaoul
+## Mail: hamzatamenaoul@gmail.com
+
 poweroff="Poweroff"
 reboot="Reboot"
 suspend="Suspend"
 
 options="$poweroff\n$reboot\n$suspend"
 
-rofi_command="rofi"
+rofi_command="rofi -theme ~/.scripts/themes/power.rasi" 
 
 chosen="$(echo -e $options | $rofi_command -dmenu -selected-row 0)"
 
