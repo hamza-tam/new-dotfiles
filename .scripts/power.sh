d31f3a4eb721f669f9561c0397e9f8d90d87c029
Hamza Tamenaoul <hamzatamenaoul@gmail.com>
2020-09-01 18:35:11 +0200

    Added lf config is bashrc. Changed font in Xresources. Added some shortcuts to i3 Added power rofi menu.

---
+++ .scripts/power.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+poweroff="Poweroff"
+reboot="Reboot"
+suspend="Suspend"
+
+options="$poweroff\n$reboot\n$suspend"
+
+rofi_command="rofi"
+
+chosen="$(echo -e $options | $rofi_command -dmenu -selected-row 0)"
+
+case $chosen in 
+  $poweroff)
+    systemctl poweroff
+    ;;
+  $reboot)
+    systemctl reboot
+    ;;
+  $suspend)
+    mpc -q pause
+    systemctl suspend
+    ;;
+esac
